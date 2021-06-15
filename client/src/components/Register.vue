6177353e6c6fdbac5eb1864c536f2d18379a3d77
deepraiya17 <deepraiya17@gmail.com>
2021-06-15 14:08:34 +0530

    updated letcure 1 code

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,9 +1,45 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <br />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <br />
+    <!-- <div class="error" v-html="error" /> -->
+    <br />
+    <br />
+
+    <button @click="register">Register</button>
   </div>
 </template>
 <script>
-export default {};
+import AuthenticationService from "../services/AuthenticationService.js";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      // error: null,
+    };
+  },
+  methods: {
+    async register() {
+      // try {
+      const respomse = await AuthenticationService.register({
+        email: this.email,
+        password: this.password,
+      });
+      console.log(respomse.data);
+      // } catch (err) {
+      //   this.error = err.response.data.error;
+      // }
+    },
+  },
+};
 </script>
 <style></style>
